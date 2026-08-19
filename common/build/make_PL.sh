5720e8ecd8f417d797118c89239ac60880529aab
Lester Lo <21245380+lesterlo@users.noreply.github.com>
2026-08-19 19:39:34 -0400

    Reserve more memory

--- common/build/make_PL.sh
+++ common/build/make_PL.sh
@@ -175,8 +175,8 @@ pl_available_memory_mb() {
 }
 
 pl_compute_auto_jobs() {
-    local job_mem="${PL_JOB_MEM_MB:-6144}"
-    local reserve_mem="${PL_RESERVE_MEM_MB:-6144}"
+    local job_mem="${PL_JOB_MEM_MB:-7168}"
+    local reserve_mem="${PL_RESERVE_MEM_MB:-8192}"
     local reserve_cpus="${PL_RESERVE_CPUS:-4}"
     local max_jobs="${PL_MAX_JOBS:-16}"
     local cpus available usable by_cpu by_mem jobs
